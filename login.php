9f724ce7fa221a3bd61cc153d7e58a4bfd714086
EduardoVaca <vacalalo@gmail.com>
2015-10-21 21:15:31 -0500

    Login first try

---
+++ login.php
@@ -0,0 +1,39 @@
+<?php
+
+  require_once "util.php";
+
+  $action = $_GET["action"];
+
+  switch ($action) {
+    case 'getUserData':
+      $username = $_GET["username"];
+      $password = $_GET["password"];
+      validateUser($username, $password);
+      break;
+
+    default:
+      # code...
+      break;
+  }
+
+
+  function validateUser($username, $password){
+    $conn = connectToDataBase();
+
+    $sql = "SELECT * FROM User WHERE userName = \"" .
+            $username . "\" AND userPassword = \"" .
+            $password . "\"";
+
+    $result = mysqli_query($conn, $sql);
+
+    closeDb($conn);
+
+    if(mysqli_num_rows($result) > 0){
+      echo "User ACCEPTED";
+    }else{
+      echo "Invalid USER";
+    }
+
+  }
+
+ ?>
