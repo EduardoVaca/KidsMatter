ac9fc802c389355927c65216351eb91f79134178
ChezzOr <chess_chris@hotmail.com>
2015-10-22 22:31:26 -0500

    Borrado de alerts del debug

--- js/renglonesBoleta.js
+++ js/renglonesBoleta.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
     $('#agregarMateria').click(function(){
         $('#boleta').append("<tr><td>Test2</td><td> <select id='"+$numL+"'><option value=''>Escolaridad</option><option value='1'>Option 1</option><option value='2'>Option 2</option><option value='3'>Option 3</option></select></td>");
         $str="#"+$numL;
-        alert($str);
         $($str).material_select();
         $numL++;
     });
