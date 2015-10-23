3d04635d61f9b60910f92dd78e511f1a1631d59c
ChezzOr <chess_chris@hotmail.com>
2015-10-22 22:28:17 -0500

    Eliminado de archivos que fueron renombrados

--- js/script.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-   $('#boletas').click(function(){
-       $('#editable').html("<table class='striped justified centered teal lighten-3 z-depth-1 tabla-actividades'><thead><tr><th data-field='id' >Materia</th><th data-field='escolaridad' >Calificacion</th></tr></thead><tbody><tr><td><p>Clase</p></td><td></td></tr><tr></tr><tr></tr></tbody></table>");
-   });
-});
