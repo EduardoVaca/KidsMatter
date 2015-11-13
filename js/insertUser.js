2766c7514387fd0f5dd1ff21b0ee29f9a518b193
EduardoVaca <vacalalo@gmail.com>
2015-11-13 16:26:44 -0600

    Inserting user in db

--- js/insertUser.js
+++ js/insertUser.js
@@ -1,8 +1,44 @@
 $(document).ready(function(){
 
+  $('#addUser').click(insertUserInDb());
   getInstitutions();
   getRoles();
 });
+
+function validate(){
+
+  if($('#nombre').val().length < 0)
+    return false;
+  if($('#password1').val().length < 0)
+    return false;
+  if($('#password1').val() != $('#password2').val())
+    return false;
+  return true;
+}
+
+
+function insertUserInDb(){
+  if(validate()){
+    alert("Fields are validate");
+
+    var userName = $('#nombre').val();
+    var passwordUser = $('#password1').val();
+    var institutionId = $('#institutionCombo').val();
+    var rolId = $('#rolesCombo').val();
+
+    alert("instId" + institutionId + " rolId:" + rolId);
+    $.post("../Controladores/insertions.php", {
+      action: "insertUser",
+      user = userName,
+      password = passwordUser,
+      instId = institutionId,
+      roleId = rolId
+    }, function(data){
+      alert(data);
+    });
+  }
+
+}
 
 function getInstitutions(){
   $.post("../Controladores/getInfo.php", {
